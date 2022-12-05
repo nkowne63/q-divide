7273ecc81f147ab088519e68cad84097f4e0c9d4
nkowne63rt <neutron63zf@gmail.com>
2022-12-06 00:14:41 +0900

    fix: code

--- src/lib.rs
+++ src/lib.rs
@@ -43,6 +43,48 @@ fn uniform_layered_internal(n: i32, count: i32) -> Vec<Vec<QubitCell>> {
     qubits_vec
 }
 
+// rだけ余分なものがある
+fn uniform_layered_internal_redundant(n: i32, count: i32, r: i32) -> Vec<Vec<QubitCell>> {
+    let mut qubits_vec = Vec::new();
+    (0..count).for_each(|_| {
+        let first_qubit = cellize(Qubit::new("first"));
+        let first_control = Qubit::control(first_qubit.clone());
+        let datas = (0..n)
+            .map(|i| cellize(Qubit::new(format!("data_{}", i).as_str())))
+            .collect::<Vec<_>>();
+        let ancillas = (0..n)
+            .map(|i| cellize(Qubit::new(format!("ancilla_{}", i).as_str())))
+            .collect::<Vec<_>>();
+        let controls = in_over_2n(n, &first_control, datas.clone(), ancillas.clone());
+        let data_length = controls.len();
+        let targets = (0..(n))
+            .map(|i| cellize(Qubit::new(format!("target_{}", i).as_str())))
+            .collect::<Vec<_>>();
+        println!("redundant_targets count: {}", r);
+        let redundant_targets = (n..(n + r))
+            .map(|i| cellize(Qubit::new(format!("redundant_target_{}", i).as_str())))
+            .map(|qc| {
+                Qubit::gate(qc.clone(), PrimitiveGate::X);
+                Qubit::gate(qc.clone(), PrimitiveGate::X);
+                qc
+            })
+            .collect::<Vec<_>>();
+        // let target_length = targets.len();
+        let random_data = generate_random_datas(data_length, 1);
+        inject_qrom_datas(targets.clone(), controls, random_data);
+
+        let mut qubits = Vec::new();
+        qubits.extend(datas.clone());
+        qubits.extend(ancillas.clone());
+        qubits.extend(targets.clone());
+        qubits.extend(redundant_targets.clone());
+        qubits.push(first_qubit.clone());
+
+        qubits_vec.push(qubits);
+    });
+    qubits_vec
+}
+
 /// Formats the sum of two numbers as string.
 #[pyfunction]
 fn sum_as_string(a: i32, b: i32) -> PyResult<String> {
@@ -70,6 +112,22 @@ fn uniform_layered(n: i32, count: i32) -> PyResult<Vec<String>> {
 #[pyo3(text_signature = "(n, count, /)")]
 fn uniform_layered_qasm(n: i32, count: i32) -> PyResult<Vec<String>> {
     let qubits_vec = uniform_layered_internal(n, count);
+
+    let qasms = qubits_vec
+        .iter()
+        .map(|qubits| {
+            let qasm_file = to_qasm(qubits.clone());
+            qasm_file.to_string()
+        })
+        .collect::<Vec<_>>();
+
+    Ok(qasms)
+}
+
+#[pyfunction]
+#[pyo3(text_signature = "(n, count, r, /)")]
+fn uniform_layered_redundant(n: i32, count: i32, r: i32) -> PyResult<Vec<String>> {
+    let qubits_vec = uniform_layered_internal_redundant(n, count, r);
 
     let qasms = qubits_vec
         .iter()
@@ -224,6 +282,7 @@ fn output_json() -> PyResult<String> {
 /// import the module.
 #[pymodule]
 fn prepare_circuit(_py: Python, m: &PyModule) -> PyResult<()> {
+    println!("version 1.0.1");
     m.add_function(wrap_pyfunction!(sum_as_string, m)?)?;
     m.add_function(wrap_pyfunction!(output_json, m)?)?;
     m.add_function(wrap_pyfunction!(test_gate, m)?)?;
@@ -232,5 +291,6 @@ fn prepare_circuit(_py: Python, m: &PyModule) -> PyResult<()> {
     m.add_function(wrap_pyfunction!(count_t_depth, m)?)?;
     m.add_function(wrap_pyfunction!(uniform_layered, m)?)?;
     m.add_function(wrap_pyfunction!(uniform_layered_qasm, m)?)?;
+    m.add_function(wrap_pyfunction!(uniform_layered_redundant, m)?)?;
     Ok(())
 }
