0f2f41ee49da2265b1209c59b0cd595146028a1c
nkowne63rt <neutron63zf@gmail.com>
2025-02-19 23:46:55 +0900

    add: control_edges and feedback_edges

--- src/fragments/circuit_fragments.rs
+++ src/fragments/circuit_fragments.rs
@@ -1,26 +1,38 @@
-use std::collections::HashMap;
-
 use crate::utils::counters::Counter;
 use super::gate_fragments::GateFragment;
 
 static GLOBAL_CIRCUIT_FRAGMENT_ID: Counter = Counter::new();
 
+use petgraph::unionfind::UnionFind;
+use std::collections::HashMap;
+
 pub struct CircuitFragment {
     pub id: usize,
     pub gate_fragments: Vec<GateFragment>,
     pub qubit_edges: HashMap<usize, usize>,
-    // TODO: union-find data for control_edges
-    // union-find + addable / deletable
+    pub control_edges: UnionFind<usize>,
+    pub feedback_edges: HashMap<usize, Vec<(usize, String)>>,
 }
 
 impl CircuitFragment {
     pub fn new(gate_fragments: Vec<GateFragment>) -> Self {
         let id = GLOBAL_CIRCUIT_FRAGMENT_ID.increment();
+        let len = gate_fragments.len();
+        let feedback_edges = {
+            let mut feedback_edges = HashMap::new();
+            for (index, fragment) in gate_fragments.iter().enumerate() {
+                if fragment.label.is_measurement() {
+                    feedback_edges.insert(index, Vec::new());
+                }
+            }
+            feedback_edges
+        };
         CircuitFragment {
             id,
-            gate_fragments,
+            gate_fragments: gate_fragments,
             qubit_edges: HashMap::new(),
-        };
-        todo!()
+            control_edges: UnionFind::new(len),
+            feedback_edges,
+        }
     }
 }
