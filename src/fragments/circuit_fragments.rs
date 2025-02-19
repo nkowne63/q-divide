44773a39dc4cb3f6c310fce659949330db1d7f49
nkowne63rt <neutron63zf@gmail.com>
2025-02-19 23:56:21 +0900

    fix: feedback edges

--- src/fragments/circuit_fragments.rs
+++ src/fragments/circuit_fragments.rs
@@ -11,28 +11,19 @@ pub struct CircuitFragment {
     pub gate_fragments: Vec<GateFragment>,
     pub qubit_edges: HashMap<usize, usize>,
     pub control_edges: UnionFind<usize>,
-    pub feedback_edges: HashMap<usize, Vec<(usize, String)>>,
+    pub feedback_edges: HashMap<Vec<usize>, Vec<(usize, String)>>,
 }
 
 impl CircuitFragment {
     pub fn new(gate_fragments: Vec<GateFragment>) -> Self {
         let id = GLOBAL_CIRCUIT_FRAGMENT_ID.increment();
         let len = gate_fragments.len();
-        let feedback_edges = {
-            let mut feedback_edges = HashMap::new();
-            for (index, fragment) in gate_fragments.iter().enumerate() {
-                if fragment.label.is_measurement() {
-                    feedback_edges.insert(index, Vec::new());
-                }
-            }
-            feedback_edges
-        };
         CircuitFragment {
             id,
             gate_fragments: gate_fragments,
             qubit_edges: HashMap::new(),
             control_edges: UnionFind::new(len),
-            feedback_edges,
+            feedback_edges: HashMap::new(),
         }
     }
 }
