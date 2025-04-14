9a2ba2dd1168aa48f73a4030aef7334fe29cb8e1
nkowne63rt <neutron63zf@gmail.com>
2025-04-14 16:04:24 +0900

    add: qasm structure

--- src/qasms.rs
+++ src/qasms.rs
@@ -1,1 +1,32 @@
-// TODO: qasm structure and validators, emitters
+use std::fmt::{Display, Formatter};
+
+use crate::circuits::QuantumCircuit;
+
+enum Gate {
+    Single(usize),
+    Double(usize, usize),
+    Measurement(usize),
+    Reset(usize),
+    If(String, Vec<Gate>)
+}
+
+struct OpenQasmCircuit {
+    qubits_count: usize,
+    classical_bits_count: usize,
+    dependent_functions: Vec<String>,
+    gates: Vec<Gate>,
+}
+
+// TODO: conversion
+impl From<QuantumCircuit> for OpenQasmCircuit {
+    fn from(circuit: QuantumCircuit) -> Self {
+        todo!()
+    }
+}
+
+// TODO: string output
+impl Display for OpenQasmCircuit {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "qasm")
+    }
+}
